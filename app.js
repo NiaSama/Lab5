5ff1b75d3a7abd17b869e93faffb6043b5b2ccd6
Tanya Wickham <TanyaKWickham@gmail.com>
2018-05-19 11:38:05 -0700

    solved problem3

--- app.js
+++ app.js
@@ -50,11 +50,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sumOfThreeNums = a + b + c;
+  var productOfThreeNums = a * b * c;
+  return [sumOfThreeNums, productOfThreeNums, a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThreeNums + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productOfThreeNums + '.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -99,7 +101,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
